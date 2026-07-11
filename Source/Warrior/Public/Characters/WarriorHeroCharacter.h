1a1affe12b0e058b353f72668e8a531b9321515b
WooShinHui <162268921+WooShinHui@users.noreply.github.com>
2026-07-11 20:34:21 +0900

    feat: [Section 2] 캐릭터 생성자 설정

--- Source/Warrior/Public/Characters/WarriorHeroCharacter.h
+++ Source/Warrior/Public/Characters/WarriorHeroCharacter.h
@@ -6,6 +6,9 @@
 #include "Characters/WarriorBaseCharacter.h"
 #include "WarriorHeroCharacter.generated.h"
 
+class USpringArmComponent;
+class UCameraComponent;
+
 /**
  * 
  */
@@ -14,6 +17,23 @@ class WARRIOR_API AWarriorHeroCharacter : public AWarriorBaseCharacter
 {
 	GENERATED_BODY()
 	
+public:
+	AWarriorHeroCharacter();
+
 protected:
 	virtual void BeginPlay() override;
+
+private:
+
+	// prama, region 정의 학습 필요
+#pragma region Components
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"));
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"));
+	UCameraComponent* FollowCamera;
+
+#pragma endregion
+
 };
