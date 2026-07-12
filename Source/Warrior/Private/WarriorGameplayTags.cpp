ef6c3c934302d2f79fc7da80ebfcd7944c6a57ea
WooShinHui <162268921+WooShinHui@users.noreply.github.com>
2026-07-12 20:01:57 +0900

    feat: [section2] GamePlayTag 추가

---
+++ Source/Warrior/Private/WarriorGameplayTags.cpp
@@ -0,0 +1,10 @@
+// Woo ShinHui All Rights Reserved
+
+
+#include "WarriorGameplayTags.h"
+
+namespace WarriorGameplayTags
+{
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move,"InputTag.Move")
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look,"InputTag.Look")
+}
