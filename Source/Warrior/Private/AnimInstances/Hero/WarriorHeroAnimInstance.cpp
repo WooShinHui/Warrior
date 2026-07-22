0a880cf73639e22978edd8d9e056fb3dbc027280
WooShinHui <162268921+WooShinHui@users.noreply.github.com>
2026-07-22 19:52:14 +0900

    feat: [Section-2] HeroCharacter AnimInstance 반영

--- Source/Warrior/Private/AnimInstances/Hero/WarriorHeroAnimInstance.cpp
+++ Source/Warrior/Private/AnimInstances/Hero/WarriorHeroAnimInstance.cpp
@@ -1,5 +1,34 @@
 // Woo ShinHui All Rights Reserved
 
-
 #include "AnimInstances/Hero/WarriorHeroAnimInstance.h"
+#include "Characters/WarriorHeroCharacter.h"
 
+// 플레이어에 관한 애님 인스턴스 클래스
+void UWarriorHeroAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	// 캐릭터가 있다면, 그 캐릭터는 AWarrioHeroCharacter 클래스로 캐스팅되어야 함
+	if (OwningCharacter)
+	{
+		OwningHeroCharacter = Cast<AWarriorHeroCharacter>(OwningCharacter);
+	}
+
+}
+
+void UWarriorHeroAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+
+	// 입력을 받아서 속도가 있다면 추적 시간을 0으로 바꾸고, 릴렉스 상태를 false 유지
+	if (bHasAcceleration) {
+		IdleElpasedTime = 0.f;
+		bShouldEnterRelaxState = false;
+	}
+	// 그게 아니라면 추적시간은 델타 타임으로 더하고, 그 델타타임 시간이 기준시간을 넘겼다면 릴렉스 상태 돌입
+	else {
+		IdleElpasedTime += DeltaSeconds;
+
+		bShouldEnterRelaxState = (IdleElpasedTime >= EnterRelaxStateThreshold);
+	}
+}
--- Source/Warrior/Public/AnimInstances/Hero/WarriorHeroAnimInstance.h
+++ Source/Warrior/Public/AnimInstances/Hero/WarriorHeroAnimInstance.h
@@ -6,6 +6,7 @@
 #include "AnimInstances/WarriorCharacterAnimInstance.h"
 #include "WarriorHeroAnimInstance.generated.h"
 
+class AWarriorHeroCharacter;
 /**
  * 
  */
@@ -14,4 +15,23 @@ class WARRIOR_API UWarriorHeroAnimInstance : public UWarriorCharacterAnimInstanc
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void NativeInitializeAnimation() override; 
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds);
+
+protected:
+	// 이 HeroAnimInstances가 할당될 캐릭터
+	UPROPERTY(VisibleDefaultsOnly, BluePrintReadOnly, Category = "AnimData|References")
+	AWarriorHeroCharacter* OwningHeroCharacter;
+
+	// 현재 릴렉스 상태인지 확인하는 bool (입력받지 않고 시간이 지났는지 확인)
+	UPROPERTY(VisibleDefaultsOnly, BluePrintReadOnly, Category = "AnimData|LocomotionData")
+	bool bShouldEnterRelaxState;
+
+	// 입력을 받지 않은 시간 기준
+	UPROPERTY(EditDefaultsOnly, BluePrintReadOnly, Category = "AnimData|LocomotionData")
+	float EnterRelaxStateThreshold = 5.f;
+
+	// idle 시간 추적 (현재 몇초가 지났는지)
+	float IdleElpasedTime;
 };
